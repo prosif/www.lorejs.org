d556b9d8ca3c80186476faed9e8639172dd2d92c
Jason Hansen <jason_hansen@outlook.com>
2018-04-22 21:15:04 -0700

    improve docs for core libraries

--- src/pages/react-router/index.js
+++ src/pages/react-router/index.js
@@ -14,17 +14,49 @@ export default (props) => {
       </p>
 
       <h3>
-        Links for Learning React-Router
+        Where does React Router show up in Lore?
       </h3>
+      <p>
+        While Lore includes <code>react-router</code> by default, new projects are only lightly coupled to it.
+        The places it shows up are:
+      </p>
       <ul>
         <li>
-          <a href="https://github.com/reactjs/react-router-tutorial">Official Tutorial</a>
+          In the <code>routes.js</code> file at the root of the project
         </li>
         <li>
-          <a href="https://github.com/reactjs/react-router/tree/master/examples">Official Examples</a>
+          In the <code>lore-hook-router</code> hook that configures <code>react-router</code>
         </li>
-        <li>???</li>
+        <li>
+          In the <code>lore-hook-react</code> hook that wraps the application with <code>Router</code>
+        </li>
       </ul>
+      <p>
+        If you wanted to replace React Router with a different routing solution you could, though Lore itself will
+        continue using it as the default for the forseeable future.
+      </p>
+
+      <h3>
+        Resources for Learning React-Router
+      </h3>
+      <p>
+        You can learn more about React Router through the
+        official <a href="https://github.com/ReactTraining/react-router/tree/v3/docs">
+        documentation</a> and <a href="https://github.com/ReactTraining/react-router/tree/v3/examples">
+        examples</a>.
+      </p>
+      <p>
+        It's important to mention though that <strong>new Lore projects include react-router v3</strong>,
+        and <strong>NOT v4</strong>. The links above go to the v3 documentation.
+      </p>
+      <p>
+        By modifying some of configuration files in your project, it's possible to replace v3 with v4, but there's
+        no formal guide explaining how to do it yet. Once there is, a link will be provided here.
+      </p>
+      <p>
+        If you're interested in learning more about v4, you can explore it on the
+        new <a href="https://reacttraining.com/react-router/web/guides/philosophy">react-router website</a>.
+      </p>
     </Template>
   );
 };
